8183f6e8b6b46a3b9dbe7e73c9040bd0717ac01d
VasylyVorobev <kurma.kurma@yandex.ru>
2020-12-27 22:52:17 +0300

    add POST request for Login Form

--- account/forms.py
+++ account/forms.py
@@ -7,24 +7,20 @@ class LoginForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #
-    #     self.fields['username'].label = 'Логин'
-    #     self.fields['password'].label = 'Пароль'
-
     def clean(self):
         username = self.cleaned_data['username']
         password = self.cleaned_data['password']
 
         if not User.objects.filter(username=username).exists():
-            raise forms.ValidationError(f"Пользователь с логином {username} не найден")
+            raise forms.ValidationError("")
         user = User.objects.filter(username=username).first()
+        # user = User.objects.get(username=username)
         if user:
             if not user.check_password(password):
-                raise forms.ValidationError("Неверный пароль")
+                raise forms.ValidationError("")
+
         return self.cleaned_data
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ['username', 'password']
--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.base import View
-
+from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
 
@@ -11,3 +12,13 @@ class LoginView(View):
         context = {'form': form}
         return render(request, 'account/login.html', context)
 
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'account/login.html', {'form': form})

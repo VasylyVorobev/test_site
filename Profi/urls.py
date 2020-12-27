593178976c515ad7b77c435ec526531858fb02a6
VasylyVorobev <kurma.kurma@yandex.ru>
2020-12-27 21:56:35 +0300

    Create a new app with authorization

--- Profi/urls.py
+++ Profi/urls.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('account/', include('account.urls')),
+    path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls'))
+
 ]
 
 
---
+++ account/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from .forms import LoginForm
+
+
+class LoginView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        context = {'form': form}
+        return render(request, 'account/login.html', context)
+
